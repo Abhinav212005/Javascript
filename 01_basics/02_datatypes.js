b08e5ddfafeb73b4190350a25ee0aa4aad852666
Abhinav212005 <abhinavrai212005@gmail.com>
2024-08-30 08:16:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// console.log(3+3) console.log("hio") // error
+
+// console.log(3+3); console.log("hio") // no error
+
+let name = "abhi"
+let age =18
+let isLoggedIn= false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value(empty(temperature context of 0))
+// undefined => value is not assigned
+// symbol => unique
+
+// object
+
+//console.log(typeof age)
+//console.log(typeof null)
+//console.log(typeof undefined)
