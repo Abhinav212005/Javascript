72ce253a72e80122f3d69c05bcefdaa0865190a8
Abhinav212005 <abhinavrai212005@gmail.com>
2025-06-20 00:56:35 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){  // {} => scope
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==  // === it checks type also, !== also
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`); => power is not defined but if you'll declare var power then it can accessed here also as var is global scope
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2"); => implicit scope
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
