0dd510f33c96b6356f88063d2c5d4bbcf9c2f28c
Abhinav212005 <abhinavrai212005@gmail.com>
2025-06-21 04:00:18 +0000

    iterations

---
+++ 05_iterations/seven.js
@@ -0,0 +1,10 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = myNumers
+                .map((num) => num * 10 )  // firstly the numbers are multiplied with 10 and then 1 is added to it and then filtered
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
