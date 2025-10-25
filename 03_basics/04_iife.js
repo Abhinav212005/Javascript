b7667981118a2feb5a675a52523033684431772e
ABHINAV RAI <abhinavrai212005@gmail.com>
2025-10-25 20:30:18 +0530

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,10 +1,12 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+//An IIFE (Immediately Invoked Function Expression) is used to execute code immediately 
+//while creating its own scope, which prevents leaking variables into the 
+//global scope and enables encapsulation and privacy for data and functions.
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})(); // semi colon is to end
 
 // ()() => here the first parenthesis is for function definition and second one is for execution
 
